94145d059f72cbdf125965963a6f7e84983cdd17
mr.pppoe <mr.pppoe@gmail.com>
2011-11-02 17:43:39 +0800

    First Commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+	</div><!-- #main -->
+
+	<div id="footer" role="contentinfo">
+		<div id="colophon">
+
+<?php
+	//get_sidebar( 'footer' );
+?>
+
+</div><!-- #wrapper -->
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+	wp_footer();
+?>
+</body>
+</html>
