2fb632647f97f897715a238671da94ed0f1886fb
mr.pppoe <mr.pppoe@gmail.com>
2011-11-13 22:04:47 +0800

    Version 0.1

--- footer.php
+++ footer.php
@@ -78,22 +78,48 @@
                 </table>
             </div>
 
+            <div class=entry>
+                <?php $args = array(
+                    'orderby'          => 'name',
+                    'order'            => 'ASC',
+                    'limit'            => -1,
+                    'category_name'    => 'Blogroll',
+                    'hide_invisible'   => 1,
+                    'show_updated'     => 0,
+                    'echo'             => 1,
+                    'categorize'       => 1,
+                    'title_li'         => __('Bookmarks'),
+                    'title_before'     => '<h2>',
+                    'title_after'      => '</h2>',
+                    'category_orderby' => 'name',
+                    'category_order'   => 'ASC',
+                    'class'            => 'linkstable',
+                );
+                ?>
+                <?php wp_list_bookmarks($args) ?>            
+            </div>
+
             <div class=list>
-                <h3> About Me </h3>
                 <p>
-					<?php if (have_posts()) : the_post();
-					the_author_description();
-					endif;
-					rewind_posts(); ?>
+                    <?php if (have_posts()) : the_post();
+                    the_author_description();
+                    endif;
+                    rewind_posts(); ?>
                 </p>
             </div>
+
 
         </div>
 
         <br clear="all"/>
 
+        <div class=seperator>
+        </div>
+
         <div class=credit>
-            <p> Theme Designed by <a href="http://haoxiang.org"><strong>Haoxiang Li</strong></a> </p>
+            <p> Theme Designed by <a href="http://haoxiang.org"><strong>Haoxiang Li</strong></a></p> 
+            <p> Credits: <a href="http://www.wpzoom.com/author/admin/"><strong>Pavel Ciorici</strong></a> and <a href=http://subtlepatterns.com/">Subtle Patterns</a></p>
+            <p> This Site is licensed under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a> </p> 
         </div>
 
     </div><!-- #lonelynights -->
